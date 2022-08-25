06e00fcaee4863b2d19a3649f0b702dd9ad04f76
Neemias Pereira <neemias.pereira3@gmail.com>
2022-08-25 17:46:31 -0300

    Exercicio de Exceptions, teste 1

---
+++ Exercicios/ExerException/ExerException/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using ExerException.Entities;
+using ExerException.Entities.Exceptions;
+
+namespace ExerException
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account date: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account acc = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdaw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(withdaw);
+
+                Console.WriteLine(acc);
+            }
+            catch (DomainException e) 
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+        }
+    }
+}
